cf808d5a9a20fc8f31edbde538cd44cb6be268e2
Irfan Zidni <irfanzidniofficial@gmail.com>
2022-10-26 13:36:42 +0700

    functions and directives

---
+++ basic-utility/tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+      },
+      spacing: {
+        13: "3.25rem",
+      },
+      fontfamily: {
+        inter: ["Inter"],
+      },
+      colors: {
+        wpu: "#bada55",
+        kopi: "#c0ffee",
+      },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+        goyang: "goyang 1s ease-in-out infinite",
+      },
+      keyframes: {
+        goyang: {
+          "0, 100%": {
+            transform: "rotate(-3deg)",
+          },
+          "50%": { transform: "rotate(3deg)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
